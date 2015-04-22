dad7577c94404fd064081539d87cca2cd1c1643c
Segii Kokhan <skok@ciklum.com>
2015-04-22 09:29:52 +0300

    App init.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularPracticeApp
+ * @description
+ * # angularPracticeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularPracticeApp', []);
