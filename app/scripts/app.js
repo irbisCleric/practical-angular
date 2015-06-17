f0c7752698dacfac746fb9ba6e4d6810dae7aa6e
Segii Kokhan <skok@ciklum.com>
2015-06-17 13:44:20 +0300

    implemented ui-router.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,15 +9,38 @@
  * Main module of the application.
  */
 var mainApp = angular
-    .module('angularPracticeApp', ['ngRoute']);
+    .module('angularPracticeApp', [
+        'ui.router'
+    ]);
 
-mainApp.config(function ($routeProvider) {
-    $routeProvider.
-        when("/home", {templateUrl: '../views/welcome.html'}).
-        when("/list", {templateUrl: '../views/list.html', controller: "UsersListCtrl"}).
-        when("/list/:id", {templateUrl: '../views/detail.html'}).
-        when("/404", {templateUrl: '../404.html'}).
-        otherwise({redirectTo: '/list'});
+mainApp.config(function ($stateProvider, $urlRouterProvider) {
+
+    // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
+    // The `when` method says if the url is ever the 1st param, then redirect to the 2nd param
+    // Here we are just setting up some convenience urls.
+    $urlRouterProvider.otherwise("/home");
+
+
+    // Use $stateProvider to configure your states.
+    $stateProvider
+        .state("home", {
+            url: "/home",
+            templateUrl: '../views/welcome.html'
+        })
+        .state("contacts", {
+            url: "/contacts",
+            templateUrl: "../views/contacts.list.html",
+            controller: "UsersListCtrl"
+        })
+        .state("detail", {
+            url: "/contacts/:itemId",
+            templateUrl: "../views/contacts.detail.html",
+            controller: "SingleUserCtrl"
+        })
+        .state("404", {
+            url: "/404",
+            templateUrl: "../404.html"
+        });
 });
 
 mainApp.controller('UsersListCtrl', ['$scope', '$http',
@@ -34,9 +57,9 @@ mainApp.controller('UsersListCtrl', ['$scope', '$http',
     }
 ]);
 
-mainApp.controller('SingleUserCtrl', ['$scope', '$http', '$routeParams',
-    function ($scope, $http, $routeParams) {
-        var url = 'http://localhost:1715/api/list/' + $routeParams.id;
+mainApp.controller('SingleUserCtrl', ['$scope', '$http', '$stateParams',
+    function ($scope, $http, $stateParams) {
+        var url = 'http://localhost:1715/api/list/' + $stateParams.itemId;
 
         $http.get(url).
             success(function (data, status, headers, config) {
@@ -48,9 +71,9 @@ mainApp.controller('SingleUserCtrl', ['$scope', '$http', '$routeParams',
         }
 ]);
 
-mainApp.controller('CommentsListCtrl', ['$scope', '$http', '$routeParams',
-    function ($scope, $http, $routeParams) {
-        var url = 'http://localhost:1715/api/comments/' + $routeParams.id;
+mainApp.controller('CommentsListCtrl', ['$scope', '$http', '$stateParams',
+    function ($scope, $http, $stateParams) {
+        var url = 'http://localhost:1715/api/comments/' + $stateParams.itemId;
 
         $http.get(url).
             success(function (data, status, headers, config) {
