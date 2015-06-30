bcca6891422f5e0b9d368600f77ffaec401a0d1c
Segii Kokhan <skok@ciklum.com>
2015-06-30 14:10:10 +0300

    webpack integration. modular vendors.

---
+++ app/scripts/vendors.js
@@ -0,0 +1,9 @@
+/**
+ * Created by skok on 30/06/15.
+ */
+module.exports = function () {
+    /* JS */
+    require('angular');
+    require('angular-module-resource');
+    require('angular-ui-router');
+};
