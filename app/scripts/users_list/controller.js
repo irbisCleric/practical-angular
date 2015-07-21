98ac6539b1dc87431f8dd78c93bd5b44e47d47bb
Segii Kokhan <skok@ciklum.com>
2015-07-21 10:12:46 +0300

    modular screens. user list service.

--- app/scripts/users_list/controller.js
+++ app/scripts/users_list/controller.js
@@ -2,17 +2,19 @@
  * Created by skok on 20/07/15.
  */
 module.exports = function (mainApp) {
-    mainApp.controller('UsersListCtrl', ['$scope', '$http',
-        function ($scope, $http) {
-            var url = 'http://localhost:1715/api/list';
+    mainApp.controller('UsersListCtrl', ['$scope', '$http', 'UserListService',
+        function ($scope, $http, UserListService) {
 
-            $http.get(url).
+            UserListService.getUsers();
+            //var url = 'http://localhost:1715/api/list';
+
+            /*$http.get(url).
                 success(function (data, status, headers, config) {
                     $scope.usersList = data;
                 }).
                 error(function (data, status, headers, config) {
                     console.log(status);
-                });
+                });*/
         }
     ]);
 };
--- app/scripts/users_list/service.js
+++ app/scripts/users_list/service.js
@@ -1,18 +1,27 @@
-mainApp.service('UserListService', ['$resource'], function ($resource) {
-    var resource = $resource('http://localhost:1715/api/list');
+module.exports = function (mainApp) {
+    mainApp.service('UserListService', ['$scope', '$http',
+        function ($scope, $http) {
+            //var resource = $resource('http://localhost:1715/api/list');
 
-    resource.get().
-        success(function (data, status, headers, config) {
-            $scope.usersList = data;
-        }).
-        error(function (data, status, headers, config) {
-            console.log(status);
-        });
-});
+                this.getUsers =  function () {
+                    return $http.get('http://localhost:1715/api/list').
+                        success(function (data, status, headers, config) {
+                            $scope.usersList = data;
+                        }).
+                        error(function (data, status, headers, config) {
+                            console.log(status);
+                        });
+                };
 
-mainApp.controller('UsersListCtrl',
-    ['$scope', 'UserListService',
-        function ($scope, UserListService) {
-
+            /*this.getUsers = function () {
+             resource.get().
+             success(function (data, status, headers, config) {
+             $scope.usersList = data;
+             }).
+             error(function (data, status, headers, config) {
+             console.log(status);
+             });
+             };*/
         }
     ]);
+};
