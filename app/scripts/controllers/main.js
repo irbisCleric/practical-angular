04fb8ecd745aca62b5ad9dbaba57bda1378f8b3c
skok <skok@ciklum.com>
2015-10-13 15:41:46 +0300

    Project setup.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name practicalAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the practicalAngularApp
+ */
+angular.module('practicalAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
