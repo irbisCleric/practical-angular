bc470d10d29e10aa1de7d72c1a6188e7e218e67a
skok <skok@ciklum.com>
2015-10-13 17:45:53 +0300

    First test.

--- test/hello.js
+++ test/hello.js
@@ -1,7 +1,17 @@
 'use strict';
 
-describe('hello', function () {
-  it ('should work', function () {
-    expect(true).toBe(true);
-  })
+describe('Hello world', function () {
+  var element;
+  var $scope;
+  beforeEach(inject(function ($compile, $rootScope) {
+    $scope = $rootScope;
+    element = angular.element('<div> {{ 2 + 2 }} </div>');
+    element = $compile(element)($rootScope);
+  }));
+
+  it ('should equal 4', function () {
+    $scope.$digest();
+    expect(element.html().toBe('4'));
+  });
+
 });
